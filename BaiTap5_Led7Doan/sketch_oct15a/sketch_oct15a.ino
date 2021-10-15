639b395ce1fcbbc1e188cf3dd551fa8ad7cc0219
Đỗ Anh Minh - 60136113 <minh.da.60cntt@ntu.edu.vn>
2021-10-15 19:30:04 +0700

    BT5

---
+++ BaiTap5_Led7Doan/sketch_oct15a/sketch_oct15a.ino
@@ -0,0 +1,33 @@
+int a=0,b=1,c=2,d=3,e=8,f=12,g=13;
+void setup() {
+  pinMode(a,OUTPUT);
+  pinMode(b,OUTPUT);
+  pinMode(c,OUTPUT);
+  pinMode(d,OUTPUT);
+  pinMode(e,OUTPUT);
+  pinMode(f,OUTPUT);
+  pinMode(g,OUTPUT);
+}
+void KHONG(){
+  digitalWrite(a,HIGH);
+  digitalWrite(b,HIGH);
+  digitalWrite(c,HIGH);
+  digitalWrite(d,HIGH);
+  digitalWrite(e,HIGH);
+  digitalWrite(f,HIGH);
+  digitalWrite(g,LOW);
+}
+void TAM(){
+  digitalWrite(a,HIGH);
+  digitalWrite(b,HIGH);
+  digitalWrite(c,HIGH);
+  digitalWrite(d,HIGH);
+  digitalWrite(e,HIGH);
+  digitalWrite(f,HIGH);
+  digitalWrite(g,HIGH);
+}
+
+void loop() {
+  KHONG();
+  //TAM();
+}
